7c7a01c62120fd8af9d2d2b3aadd4dbad510c4bd
DAVID ALONSO <d.alonso.sanchez@accenture.com>
2021-03-02 22:06:24 +0100

    Código Tutorial Spring Boot básico

---
+++ spring-boot-basico/src/main/java/es/micodigobasico/springbootbasico/SpringbootBasicoApplication.java
@@ -0,0 +1,13 @@
+package es.micodigobasico.springbootbasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBasicoApplication.class, args);
+	}
+
+}
